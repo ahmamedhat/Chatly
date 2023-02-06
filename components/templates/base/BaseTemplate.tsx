d539d91d416c806fcae84074c2912631e2570a7c
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-06 12:03:30 +0200

    style: add different screen sized to tailwind configuration

--- components/templates/base/BaseTemplate.tsx
+++ components/templates/base/BaseTemplate.tsx
@@ -6,7 +6,7 @@ export interface IBaseTemplate {
 
 const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
   return (
-    <div className="bg-blue-300 inline-block p-2 rounded-2xl">
+    <div className="bg-gradient-to-r from-blue-600 to-cyan-500 inline-block p-2 px-3 rounded-2xl cursor-pointer text-white shadow-md shadow-blue-200">
       <p>{sampleTextProp}</p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./components/**/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "375px",
+        sm: "600px",
+        md: "900px",
+        lg: "1200px",
+        xl: "1536px",
+      },
+    },
   },
   plugins: [],
 };
