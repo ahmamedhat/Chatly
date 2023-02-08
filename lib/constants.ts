93ac7ca26611852c29d2c0d90e5a0b03d36747a7
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-08 13:18:16 +0200

    build: add demo images domain in next cofig & add dummy messages data

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const IconsSizes = {
   sm: "24",
-  md: "28",
+  md: "26",
   lg: "42",
 };
 
@@ -10,3 +10,42 @@ export const Colors = {
   secondaryMessage: "#d8d8d8",
   green: "#39ff5a",
 };
+
+export const Chats = [
+  {
+    id: 0,
+    name: "Zikoo 👨🏻",
+    message: "balash khara haa 🥶",
+    time: "5:13 PM",
+    image:
+      "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    chatID: 0,
+  },
+  {
+    id: 1,
+    name: "Mohanad Kamal",
+    message: "walaaa",
+    time: "4:30 PM",
+    image:
+      "https://images.pexels.com/photos/3290888/pexels-photo-3290888.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    chatID: 1,
+  },
+  {
+    id: 2,
+    name: "Ibrahim saleh",
+    message: "eh yabny fenak?",
+    time: "11:46 AM",
+    image:
+      "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    chatID: 2,
+  },
+  {
+    id: 3,
+    name: "Soheir",
+    message: "ekhlas ba2a 3shan el deadline!",
+    time: "Yesterday",
+    image:
+      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    chatID: 3,
+  },
+];
