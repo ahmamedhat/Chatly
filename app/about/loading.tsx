65280e6b97bb21a2b9fa7a5bf83f3142cfc0911d
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-02-27 05:09:01 +0200

    feat: react loading skeleton package and use nextjs new loading page

---
+++ app/about/loading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+
+const loading = () => {
+  return (
+    <div className="bg-white dark:bg-dark h-screen">
+      <Skeleton
+        baseColor="#10171E"
+        highlightColor="#15202B"
+        className="h-6 mb-4"
+      />
+      <Skeleton
+        baseColor="#10171E"
+        highlightColor="#15202B"
+        className="h-24 mb-2"
+      />
+      <Skeleton baseColor="#10171E" highlightColor="#15202B" className="h-24" />
+    </div>
+  );
+};
+
+export default loading;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,36 @@
-export default function About() {
+const fetchData = async () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("done!");
+    }, 4000);
+  });
+};
+
+export default async function About() {
+  const data = await fetchData();
+  console.log("data is", data);
+
   return (
-    <div className="bg-white dark:bg-dark h-screen flex justify-center items-center">
-      <p className="text-red-400">About</p>
+    <div className="bg-white dark:bg-dark h-screen">
+      <p className="text-gray-400 mb-6">About</p>
+      <p className="mb-2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries.
+      </p>
     </div>
   );
 }
