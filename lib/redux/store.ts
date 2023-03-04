308374256d9316e70f421386c0a95ccfe1a293e5
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-03-04 10:58:24 +0200

    chore: remove socket slice from persisted storage to prevent unwanted behaviours

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -14,6 +14,7 @@ const reducer = combineReducers({
 const persistConfig = {
   key: "root",
   storage,
+  blacklist: ["socket"],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
