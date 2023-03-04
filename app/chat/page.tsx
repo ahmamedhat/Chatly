80b36b4c3af5153abfc8fc426abdff8fe38e56fe
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-03-04 09:24:30 +0200

    feat: fetch all chats and message on the server to optimize performance

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { ChatHeader, ChatMain } from "@/components";
+import client from "@/lib/api/apollo";
+import { GET_CHAT } from "@/lib/api/query";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
-import React from "react";
 
 export default async function Chat({
   searchParams,
@@ -9,6 +11,20 @@ export default async function Chat({
   searchParams?: { [key: string]: string | undefined };
 }) {
   const session = await getServerSession(authOptions);
+  let chat;
+
+  if (searchParams?.chatID) {
+    try {
+      const response = await client.query({
+        query: GET_CHAT,
+        variables: { chatId: searchParams?.chatID },
+        fetchPolicy: "no-cache",
+      });
+      chat = response.data.chat;
+    } catch (e) {
+      console.log("error is", e);
+    }
+  }
 
   return (
     <div className="bg-white dark:bg-dark h-full absolute inset-0">
@@ -17,6 +33,7 @@ export default async function Chat({
         <ChatMain
           currentUser={session.user}
           onlineUserID={searchParams?.userID as string}
+          chat={chat}
         />
       </div>
     </div>
--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,12 +1,19 @@
-import { OnlineUsers } from "@/components";
+import { ChatsHistory, OnlineUsers } from "@/components";
+import { GET_CHATS } from "@/lib/api/query";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
+import client from "@/lib/api/apollo";
 
 export default async function Messages() {
   const session = await getServerSession(authOptions);
-
+  const { data } = await client.query({
+    query: GET_CHATS,
+    variables: { userId: session.user.id },
+    fetchPolicy: "no-cache",
+  });
   return (
     <div className="h-full">
+      <ChatsHistory currentUser={session?.user} chats={data.chats} />
       <OnlineUsers currentUser={session?.user} />
     </div>
   );
