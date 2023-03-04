6bd390e576b8b198c48b273897c3478c5bc9c371
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-03-04 09:28:17 +0200

    feat: now sending messages is sent to database

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -1,31 +1,46 @@
 "use client";
 
-import { ChatMessage, User } from "@/types/typings";
-import { ChatMessage as ChatMessageComponent } from "@/components";
+import { User } from "@/types/typings";
+import {
+  ChatMessage as ChatMessageComponent,
+  MessageInput,
+} from "@/components";
 import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { socketActions } from "@/lib/redux/reducers/socketSlice";
 import { RootState } from "@/lib/redux/store";
-import { TbSend } from "react-icons/tb";
-import { IconsSizes } from "@/lib/constants";
-import clsx from "clsx";
+import { useMutation } from "@apollo/client";
+import { ADD_NEW_MESSAGE } from "@/lib/api/mutation";
+import { Chat, Message } from "@/lib/api/gql/graphql";
+import moment from "moment";
 
 interface IChatMessages {
   currentUser: User;
   onlineUserID: string;
+  chatID?: string;
+  chat?: Chat;
 }
 
-const ChatMessages = ({ currentUser, onlineUserID }: IChatMessages) => {
+const ChatMessages = ({ currentUser, onlineUserID, chat }: IChatMessages) => {
   const dispatch = useDispatch();
   const chatsState = useSelector((store: RootState) => store.socket.chats);
-  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
-  const [messageInput, setMessageInput] = useState<string>("");
-
+  const [chatMessages, setChatMessages] = useState<Message[]>([]);
   const chatList = useRef<null | HTMLDivElement>(null);
 
+  const [addNewMessage] = useMutation(ADD_NEW_MESSAGE);
+
   useEffect(() => {
+    if (chat?.messages) {
+      setChatMessages(chat.messages);
+      dispatch(
+        socketActions.receiveAllMessages({
+          userID: onlineUserID,
+          chat: chat,
+        })
+      );
+    }
     if (currentUser) {
-      dispatch(socketActions.startConnecting());
+      dispatch(socketActions.startConnecting(currentUser));
       return () => {
         dispatch(socketActions.disconnect());
       };
@@ -33,33 +48,44 @@ const ChatMessages = ({ currentUser, onlineUserID }: IChatMessages) => {
   }, []);
 
   useEffect(() => {
+    chatsState[onlineUserID].length &&
+      setChatMessages(chatsState[onlineUserID]);
     chatList.current?.scrollIntoView({ behavior: "smooth" });
-    chatsState?.length && setChatMessages(chatsState[0]?.messages);
-  }, [chatsState[0]?.messages]);
+  }, [chatsState[onlineUserID]]);
 
-  const sendMessage = () => {
+  const sendMessage = (messageInput: string) => {
     if (messageInput.trim().length < 1) return;
-    const newMessage: ChatMessage = {
-      message: messageInput,
-      id: Date.now(),
-      receiverId: onlineUserID ?? "2",
-      senderId: currentUser.id ?? "1",
-      time: Date().toString(),
-    };
+    try {
+      addNewMessage({
+        variables: {
+          from: currentUser.id,
+          to: onlineUserID,
+          body: messageInput,
+        },
+      });
+    } catch (e) {
+      console.log("an error has occured");
+    }
 
-    const uid =
-      newMessage.senderId === currentUser?.id
-        ? newMessage.receiverId
-        : newMessage.senderId;
+    const newMessage: Message = {
+      _id: moment().valueOf().toString(),
+      body: messageInput,
+      //@ts-ignore
+      to: { _id: onlineUserID },
+      //@ts-ignore
+      from: { _id: currentUser.id },
+      createdAt: moment().valueOf().toString(),
+    };
     dispatch(
       socketActions.submitMessage({
         message: newMessage,
         room: onlineUserID as string,
       })
     );
-    dispatch(socketActions.receiveMessage({ message: newMessage, uid }));
+    dispatch(
+      socketActions.receiveMessage({ message: newMessage, uid: onlineUserID })
+    );
     setChatMessages([...chatMessages, newMessage]);
-    setMessageInput("");
   };
 
   return (
@@ -68,42 +94,17 @@ const ChatMessages = ({ currentUser, onlineUserID }: IChatMessages) => {
         return (
           <ChatMessageComponent
             currentUserId={currentUser.id ?? "1"}
-            id={chatMessage.id}
-            message={chatMessage.message}
-            receiverId={chatMessage.receiverId}
-            senderId={chatMessage.senderId}
-            time={chatMessage.time}
-            key={chatMessage.id}
+            _id={chatMessage._id}
+            body={chatMessage.body}
+            from={chatMessage.from}
+            to={chatMessage.to}
+            createdAt={chatMessage.createdAt}
+            key={chatMessage._id}
           />
         );
       })}
-      <div className="my-auto" />
-      <div ref={chatList} />
-      <div className="fixed mt-4 mx-auto left-2 right-2 bottom-4 max-w-[60rem]">
-        <input
-          type="text"
-          value={messageInput}
-          onChange={(e) => setMessageInput(e.target.value)}
-          placeholder="Write your message..."
-          className="input w-full bg-lightTextInput dark:bg-darkTextInput text-offBlack dark:text-secondaryMessage"
-          onKeyDown={(event) => {
-            if (event.key == "Enter") sendMessage();
-          }}
-          required={true}
-        />
-
-        <TbSend
-          size={IconsSizes.lg}
-          className={clsx(
-            "absolute right-2 top-[50%] -translate-y-[50%] cursor-pointer duration-200 select-none transition rounded-full p-1 stroke-white",
-            {
-              "dark:bg-darkMessage bg-gray-500": messageInput.trim().length < 1,
-              "bg-primaryMessage": messageInput.trim().length > 0,
-            }
-          )}
-          onClick={sendMessage}
-        />
-      </div>
+      <div ref={chatList} className="pb-16" />
+      <MessageInput onSendHandler={(message) => sendMessage(message)} />
     </div>
   );
 };
