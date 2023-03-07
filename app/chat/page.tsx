302e5648cb66d4b644a0d822cd3c58eba793206a
ahmadmed7at77 <ahmadmed7at77@gmail.com>
2023-03-07 02:49:59 +0200

    fix: app crashes when user is not logged in and try to enter a chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -15,41 +15,45 @@ export default async function Chat({
   let chat;
   let user;
 
-  if (searchParams?.chatID) {
-    try {
-      const response = await client.query({
-        query: GET_CHAT,
-        variables: { chatId: searchParams?.chatID },
-        fetchPolicy: "no-cache",
-      });
-      chat = response.data.chat;
-      user = parseUser(chat.users, session.user.id);
-    } catch (e) {
-      console.log("error is", e);
-    }
-  } else {
-    try {
-      const response = await client.query({
-        query: GET_USER,
-        variables: { userId: searchParams?.userID },
-        fetchPolicy: "no-cache",
-      });
-      user = response.data.user;
-    } catch (e) {
-      console.log("error is", e);
+  if (session?.user) {
+    if (searchParams?.chatID) {
+      try {
+        const response = await client.query({
+          query: GET_CHAT,
+          variables: { chatId: searchParams?.chatID },
+          fetchPolicy: "no-cache",
+        });
+        chat = response.data.chat;
+        user = parseUser(chat.users, session?.user?.id);
+      } catch (e) {
+        console.log("error is", e);
+      }
+    } else {
+      try {
+        const response = await client.query({
+          query: GET_USER,
+          variables: { userId: searchParams?.userID },
+          fetchPolicy: "no-cache",
+        });
+        user = response.data.user;
+      } catch (e) {
+        console.log("error is", e);
+      }
     }
   }
 
   return (
     <div className="bg-white dark:bg-dark h-full absolute inset-0">
-      <div className="max-w-[60rem] flex flex-col justify-between pb-6 mx-auto h-full ">
-        <ChatHeader user={user} />
-        <ChatMain
-          currentUser={session.user}
-          onlineUserID={searchParams?.userID as string}
-          chat={chat}
-        />
-      </div>
+      {session?.user && (
+        <div className="max-w-[60rem] flex flex-col justify-between pb-6 mx-auto h-full ">
+          <ChatHeader user={user} />
+          <ChatMain
+            currentUser={session?.user}
+            onlineUserID={searchParams?.userID as string}
+            chat={chat}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -6,14 +6,19 @@ import client from "@/lib/api/apollo";
 
 export default async function Messages() {
   const session = await getServerSession(authOptions);
-  const { data } = await client.query({
-    query: GET_CHATS,
-    variables: { userId: session.user.id },
-    fetchPolicy: "no-cache",
-  });
+  let chats;
+
+  if (session?.user) {
+    const response = await client.query({
+      query: GET_CHATS,
+      variables: { userId: session?.user?.id },
+      fetchPolicy: "no-cache",
+    });
+    chats = response.data.chats;
+  }
   return (
     <div className="h-full">
-      <ChatsHistory currentUser={session?.user} chats={data.chats} />
+      <ChatsHistory currentUser={session?.user} chats={chats} />
       <OnlineUsers currentUser={session?.user} />
     </div>
   );
